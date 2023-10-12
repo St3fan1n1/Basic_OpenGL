308c678b4d7151fe4f89a001ad64fa214ecd0f0a
Stefano Romanini <spromanini@miuandes.cl>
2023-10-12 18:37:42 -0300

    Window resize and clear color

--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@
 #include <iostream>
 #include <string>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 int main()
 {
     // Initiate GLFW
     if (!glfwInit())
     {
+        std::cerr << "Error: Failed to initiate GLFW." << std::endl;
         return -1;
     }
 
@@ -22,11 +25,13 @@ int main()
 
     if (!window)
     {
+        std::cerr << "Error: Failed to create GLFW window." << std::endl;
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // Initiate GLEW
     GLenum err = glewInit();
@@ -40,11 +45,25 @@ int main()
     // Main loop
     while (!glfwWindowShouldClose(window))
     {
+        // Input
+
+        // Rendering code
+        glClearColor(0.21f, 0.19f, 0.38f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Call events and swap buffer
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     // Cleanup
+    glfwDestroyWindow(window);
     glfwTerminate();
 
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
